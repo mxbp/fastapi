366e2fcff6d9b1d8517be0519c5d84550eb69c3e
Maxim <mxmbzp@gmail.com>
2025-02-21 15:27:58 -0800

    feat: add conn env

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 from typing import List
 
 import asyncpg
@@ -19,7 +20,13 @@ async def read_root():
 
 
 async def get_db():
-    conn = await asyncpg.connect("postgresql://user:password@hostname:port/dbname")
+    postgres_user = os.getenv("POSTGRES_USER", "postgres")
+    postgres_pass = os.getenv("POSTGRES_PASS", "postgres")
+    postgres_host = os.getenv("POSTGRES_HOST", "localhost")
+    postgres_port = os.getenv("POSTGRES_PORT", "5432")
+    postgres_db = os.getenv("POSTGRES_DB", "postgres")
+
+    conn = await asyncpg.connect(f"postgresql://{postgres_user}:{postgres_pass}@{postgres_host}:{postgres_port}/{postgres_db}")
     try:
         yield conn
     finally:
